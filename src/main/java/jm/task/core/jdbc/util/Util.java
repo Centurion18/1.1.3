ba3e964845a1790d08cfe339679664544ba23fcf
Centurion18 <147394112+Centurion18@users.noreply.github.com>
2024-03-13 15:28:13 +0300

    1.1.3 JDBC AND HIBERNATE

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static String username = "postgres";
+    private static String password = "SIMsim01";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+
 }
